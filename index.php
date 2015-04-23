a5e205af0dcce78715ffdbc07159c01706cf92b2
Luis Cedano <king.romanus1@gmail.com>
2015-04-23 11:22:18 -0700

    Milestone 6

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Luis To-do 2.0</title>
+	<title>To-Do List 2</title>
 	<link rel="stylesheet" type="text/css" href="css/mainn.css">
 </head>
 <body>
 	<div class="wrap">
-
+		<div class="task-list">
+				<ul>
+					<?php require("includes/connect.php"); ?>
+				</ul>
+		</div>
+		<form class="add-new-task" autocomplete="off">
+			<input type="text" name="new-task" placeholder="Add new item..."/>
+		</form>
 	</div>
 </body>
 </html>
