6e62a74498c8586dbdee8b6cdebd55bd3b3170ab
Luis Cedano <king.romanus1@gmail.com>
2015-04-23 11:13:45 -0700

    Milestone 2 completed

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Luis To-do 2.0</title>
+	<link rel="stylesheet" type="text/css" href="css/mainn.css">
+</head>
+<body>
+	<div class="wrap">
+
+	</div>
+</body>
+</html>
