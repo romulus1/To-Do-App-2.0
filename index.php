b8b940bfadde9edf548e74912b484d80d5503f41
Luis Cedano <king.romanus1@gmail.com>
2015-04-24 18:49:54 -0700

    Milestone 9 complete

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>To-Do List 2</title>
-	<link rel="stylesheet" type="text/css" href="css/mainn.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
 	<div class="wrap">
@@ -16,4 +16,20 @@
 		</form>
 	</div>
 </body>
+<script src="https://code.jquery.com/jquery-latest.min.js"></script>
+<script>
+	add_task(); // calling the add task function
+	function add_task() {
+		$('.add-new-task').submit(function() {
+			var new_task = $('.add-new-task input[name=new-task]').val();
+			if(new_task != '') {
+				$.post('includes/add-task.php', {task: new_task}, function(data) {
+					$('add-new-task input[name=new-task]').val();
+						$(data).appendTo('task-list ul').hide().fadeIn();
+				});
+			}
+			return false;
+		});
+	}
+</script>
 </html>
