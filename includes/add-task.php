44686ac576c2701bd35ca929997189ead45faaa2
Luis Cedano <king.romanus1@gmail.com>
2015-04-24 18:51:54 -0700

    Milestone 12 complete

--- includes/add-task.php
+++ includes/add-task.php
@@ -6,4 +6,12 @@
 	$mysqli = new mysqli('localhost', 'root', 'root', 'todo2');
 	$mysqli->query("INSERT INTO task VALUES ('", "$task", "$date", "$time')");
 	$query = "SELECT * FROM task WHERE task='$task' and date='$date' and time='$time'";
+	if($result = $mysqli->query($query)) {
+		while ($row = $result->fetch_assoc()) {
+			$task_id = $row['id'];
+			$task_name = $row['task'];
+		}
+	}
+	$mysqli->close();
+	echo '<li><span>'.$task_name.'</span><img id="'.$task_id.'" class="delete-button" width="10px" src="images/close.svg" /></li>';
 ?>
