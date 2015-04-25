45b6cdce07e4c217594333ef0554546bf0db6390
Luis Cedano <king.romanus1@gmail.com>
2015-04-24 18:54:35 -0700

    Milestone 16 complete

--- index.php
+++ index.php
@@ -17,9 +17,10 @@
 							while($row = $result->fetch_assoc()){
 								$task_id = $row['id'];
 								$task_name = $row['task'];
-								echo "<li>
-									<span>'.$task_name.'
-									";
+								echo '<li>
+								<span>'.$task_name. '</span>
+								<img id="'.$task_id.'" class = "delete-button" width="10px" src="images/close.svg"/>
+								</li>';
 							}
 						}
 					}
